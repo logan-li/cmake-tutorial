7cf33b86a42c45efa458f6384309d246dba1c72f
hao li <lihao1q87@qq.com>
2018-06-09 18:13:57 +0800

    multi file in single directory

---
+++ math.h
@@ -0,0 +1,8 @@
+/**
+ * power - Calculate the power of number.
+ * @param base: Base value.
+ * @param exponent: Exponent value.
+ *
+ * @return base raised to the power exponent.
+ */
+double power(double base, int exponent);
